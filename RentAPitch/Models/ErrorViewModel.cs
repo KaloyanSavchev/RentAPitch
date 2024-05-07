6ee3e52b4acf578617c6715ad3ef0eccedd5abc3
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-07 11:34:26 +0300

    First Commit

---
+++ RentAPitch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentAPitch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
