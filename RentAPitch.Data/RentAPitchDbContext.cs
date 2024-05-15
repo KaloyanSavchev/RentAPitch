8ef44fbc2497b0fde810535b636067a055ade247
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-15 11:42:05 +0300

    Data project created

---
+++ RentAPitch.Data/RentAPitchDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RentAPitch.Data
+{
+    public class RentAPitchDbContext : IdentityDbContext
+    {
+        public RentAPitchDbContext(DbContextOptions<RentAPitchDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
