6febcd1d6161ced80c819900d7d94ebb1fda1bc1
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:11:24 +0300

    Utilities for the images added + needs to be checked wrong reading of image files

---
+++ RentAPitch/Utility/ImageUpload.cs
@@ -0,0 +1,34 @@
+﻿namespace RentAPitch.Utility
+{
+    public class ImageUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public string SaveImageFile(IFormFile ImageUrl)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string uploadPath = Path.Combine(webRootPath, "upload");
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageUrl.FileName); // Error
+            var filePath = Path.Combine(uploadPath, fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                ImageUrl.CopyTo(fileStream);
+            }
+            return Path.Combine("upload", fileName);
+        }
+
+
+
+    }
+}
