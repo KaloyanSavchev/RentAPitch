e0a9d5877672e809831229e210cfe770d60c864d
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:00:21 +0300

    Create the class with infrastructure of the Pitch Repo

---
+++ RentAPitch.Repositories/Infrastructure/IPitchRepository.cs
@@ -0,0 +1,22 @@
+﻿using RentAPitch.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAPitch.Repositories.Infrastructure
+{
+    public interface IPitchRepository
+    {
+        Task<IEnumerable<Pitch>> GetPitches();
+
+        Task<Pitch> GetPitchById(int id);
+
+        Task InsertPitches(Pitch pitch);
+
+        Task UpdatePitches(Pitch pitch);
+
+        Task DeletePitches(int id);
+    }
+}
