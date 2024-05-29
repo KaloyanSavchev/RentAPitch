d3f973fed70852db953101a330d27a53eaffbf56
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:09:30 +0300

    Mapper for the pitch images

---
+++ RentAPitch/Mapper/PitchProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using RentAPitch.Data.Models;
+using RentAPitch.Models.ViewModels.Pitch;
+using RentAPitch.Utility;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RentAPitch.Mapper
+{
+    public class PitchProfile : Profile
+    {
+        private  IWebHostEnvironment _WebHostEnvironment;
+        
+        public PitchProfile(IWebHostEnvironment webHostEnvironment)
+        {
+            _WebHostEnvironment = webHostEnvironment;
+
+            CreateMap<Pitch, PitchViewModel>();
+            CreateMap<CreatePitchViewModel, Pitch>()
+                .ForMember(destination => destination.ImageUrl,
+                opt => opt.MapFrom(source => new ImageUpload(_WebHostEnvironment).SaveImageFile(source.PitchImageUrl)));
+
+
+        }
+    }
+}
