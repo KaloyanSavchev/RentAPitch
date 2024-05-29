b207d0224c3d7ad63cd367ab43088b5c99174aff
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:12:23 +0300

    Changes in the Program.cs file

--- RentAPitch/Program.cs
+++ RentAPitch/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using RentAPitch.CustomMiddleWare;
 using RentAPitch.Data;
+using RentAPitch.Data.Models;
+using RentAPitch.Mapper;
+using RentAPitch.Repositories.Implementation;
+using RentAPitch.Repositories.Infrastructure;
 
 namespace RentAPitch
 {
@@ -16,11 +21,31 @@ namespace RentAPitch
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole<Guid>>()
                 .AddEntityFrameworkStores<RentAPitchDbContext>();
-            builder.Services.AddControllersWithViews();
 
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new PitchProfile(builder.Environment));
+            });
+            var mapper = config.CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = "RentAPitchProjectCookie";
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
+                options.Cookie.HttpOnly = true;
+            });
+
+            builder.Services.AddScoped<IPitchRepository, PitchRepository >();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IPitchRepository, PitchRepository>();
+            builder.Services.AddAutoMapper(typeof(PitchRepository));
             var app = builder.Build();
+
+
 
            
             if (app.Environment.IsDevelopment())
@@ -37,13 +62,15 @@ namespace RentAPitch
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Pitch}/{action=Index}/{id?}");
+            
             app.MapRazorPages();
 
             app.Run();
