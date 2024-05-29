0572aa74f56678d35b64c0fe7fdc0a0feb71d0cf
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:05:56 +0300

    Fixed the aplication user problem - Information is now provided from IdentityUser

---
+++ RentAPitch.Data.Models/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentAPitch.Data.Models
+{
+    using static Constants.EntityConstants.UserConstants;
+
+    public class ApplicationUser : IdentityUser<Guid>
+    {      
+        //[Required]
+        //[StringLength(UserNameMaxLength)]
+        //public string UserName { get; set; } = null!;
+        //[Required]
+        //[DataType(DataType.Password)]
+        //public string Password { get; set; } = null!;
+
+        //[Required]
+        //[StringLength(PhoneNumberMaxLenght)]
+        //public int PhoneNumber { get; set; }
+
+        //public bool IsAdmin { get; set; }
+
+        public ICollection<Reservation> Reservations { get; set; } = new List<Reservation>();
+
+    }
+}
