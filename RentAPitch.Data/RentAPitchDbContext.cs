2cd4eb4cd64bad7e145994103c3c47b7f77c7dfb
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:06:53 +0300

    Small changes in the DbContext

--- RentAPitch.Data/RentAPitchDbContext.cs
+++ RentAPitch.Data/RentAPitchDbContext.cs
@@ -1,25 +1,22 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using RentAPitch.Data.Models;
 
 namespace RentAPitch.Data
 {
-    public class RentAPitchDbContext : IdentityDbContext
+    public class RentAPitchDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
-
-        public DbSet<User> Users {  get; set; }
-        public DbSet<Pitch> Pitches { get; set; }
-        public DbSet<Reservation> Reservations { get; set; }
         public RentAPitchDbContext(DbContextOptions<RentAPitchDbContext> options)
             : base(options)
         {
 
         }
 
-       /* protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.Entity<Pitch>()
-                .HasOne(h => h.)
-        }*/
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } = null!;
+        public DbSet<Pitch> Pitches { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+       
     }
 }
