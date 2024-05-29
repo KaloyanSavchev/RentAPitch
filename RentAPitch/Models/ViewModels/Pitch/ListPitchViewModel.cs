c2a6b838b4b8b2a3659a5d832b841e0943193ebb
KaloyanSavchev <kaloyan.savchev06@gmail.com>
2024-05-29 19:10:05 +0300

    ViewModels Created for the Pitch

---
+++ RentAPitch/Models/ViewModels/Pitch/ListPitchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RentAPitch.Utility;
+
+namespace RentAPitch.Models.ViewModels.Pitch
+{
+    public class ListPitchViewModel
+    {
+        public IEnumerable<PitchViewModel> PitchList { get; set; }
+
+        public PageInfo PageInfo { get; set; }
+        public string SearchingText { get; set; }
+    }
+}
---
+++ RentAPitch/Models/ViewModels/Pitch/PitchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentAPitch.Models.ViewModels.Pitch
+{
+    public class PitchViewModel
+    {
+        public int Id { get; set; }     
+        public string PitchName { get; set; } = null!;
+        public string Location { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public decimal PricePerDay { get; set; }
+        public bool IsAvailable { get; set; }
+        public bool IsDelete { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
